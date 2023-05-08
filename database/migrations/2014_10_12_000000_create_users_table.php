289586c7dabb02099f3a6380c67e74b3d3b3982f
ThanHtike <cgm.thanhtike@gmail.com>
2023-05-08 16:56:14 +0630

    first commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->text('profile');
+            $table->tinyInteger('role')->default(2);
+            $table->string('phone')->nullable();
+            $table->date('birthday')->nullable();
+            $table->string('address')->nullable();
+            $table->bigInteger('created_user_id');
+            $table->bigInteger('updateed_user_id');
+            $table->bigInteger('deleted_user_id')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
